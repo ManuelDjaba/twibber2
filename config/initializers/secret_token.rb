8d63cdf6f27ec2e982e6d95bf18c070fcf0fb4f3
Emmanuel Djaba <e.djaba@bslglobal.com>
2013-10-03 15:59:30 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twibber2::Application.config.secret_token = '6b7cb2e1e4ed3116af49fe29dd868eabcf6eda2af95f6ed1f945760ad27d9ba4038fcdfcec2edfef5b0ba98a9f2b77f9e286fcc5b28987296c37999928ed39be'
